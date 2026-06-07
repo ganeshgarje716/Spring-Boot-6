ed11f07c4a8d27a97588273829fcad1e600d4af9
Ganesh <ganeshgarje716@gmail.com>
2026-06-07 18:27:18 +0530

    Create welcome() Method and add @GetMapping Annotation on method and add @Controller Annotation on WelcomeController class

--- SpringBoot-6/src/main/java/com/ganesh/controller/WelcomeController.java
+++ SpringBoot-6/src/main/java/com/ganesh/controller/WelcomeController.java
@@ -1,8 +1,23 @@
 package com.ganesh.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
+	
+	
+	@GetMapping("/welcome")
+	public ModelAndView welcome() {
+		
+		ModelAndView mav=new ModelAndView();
+		
+		mav.setViewName("index");
+		
+		return mav;
+	}
+	
+	
 
 }
